950543044050f33feadd265983f675cb7b93d980
3ndr1ck <104274745+3ndr1ck@users.noreply.github.com>
2025-08-27 05:24:01 +0200

    correction de hash de décryptage

--- checkPassword.php
+++ checkPassword.php
@@ -2,11 +2,14 @@
 // checkPassword.php
 header('Content-Type: application/json');
 
-$storedPassword = trim(file_get_contents('/home/vpsfyhi/.password')); // chemin sécurisé
-$entered = $_POST['password'] ?? '';
+// chemin sécurisé
+$storedPasswordHash = trim(file_get_contents('/home/vpsfyhi/.password')); 
+$enteredPassword = $_POST['password'] ?? '';
 
-if ($entered === $storedPassword) {
+// On utilise password_verify() pour comparer le mot de passe en clair avec le hachage
+if (password_verify($enteredPassword, $storedPasswordHash)) {
     echo json_encode(['success' => true]);
 } else {
     echo json_encode(['success' => false]);
 }
+?>
